53d6d74c198c0bfab78d2deb4795bace1a7205da
Tharuni-2310 <tharunichinnam@gmail.com>
2025-07-11 17:20:56 +0530

    fruit

--- app.py
+++ app.py
@@ -1,41 +1,58 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import load_model
 import streamlit as st
 import numpy as np
-import os
+model = load_model('C:\Fruits-Image\Image_classify.keras')
 
-# Load model (ensure you use raw string for Windows path)
-model = load_model(r'C:\Fruits-Image\Image_classify.keras')
 
-# Category labels
-data_cat = [
-    'apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot',
-    'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger',
-    'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange',
-    'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish',
-    'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon'
-]
+data_cat=['apple',
+ 'banana',
+ 'beetroot',
+ 'bell pepper',
+ 'cabbage',
+ 'capsicum',
+ 'carrot',
+ 'cauliflower',
+ 'chilli pepper',
+ 'corn',
+ 'cucumber',
+ 'eggplant',
+ 'garlic',
+ 'ginger',
+ 'grapes',
+ 'jalepeno',
+ 'kiwi',
+ 'lemon',
+ 'lettuce',
+ 'mango',
+ 'onion',
+ 'orange',
+ 'paprika',
+ 'pear',
+ 'peas',
+ 'pineapple',
+ 'pomegranate',
+ 'potato',
+ 'raddish',
+ 'soy beans',
+ 'spinach',
+ 'sweetcorn',
+ 'sweetpotato',
+ 'tomato',
+ 'turnip',
+ 'watermelon']
+img_height=180
+img_width=180
+st.header('Image Classification Model')
+image = st.text_input('Enter image name saved in your device:','corn.jpg')
+image_load= tf.keras.utils.load_img(image,target_size=(img_height,img_width))
+img_arr=tf.keras.utils.array_to_img(image_load)
+img_bat=tf.expand_dims(img_arr,0)
+predict = model.predict(img_bat)
+score=tf.nn.softmax(predict)
+st.image(image,width=200)
+st.write('Veg/Fruit in image is '+data_cat[np.argmax(score)])
+st.write('The accuracy of '+ str(np.max(score)*100))
 
-img_height = 180
-img_width = 180
 
-st.header('Fruit & Vegetable Image Classification')
-image_name = st.text_input('Enter image file name (e.g., corn.jpg):', 'corn.jpg')
-
-# You can either hardcode or let user upload (optional)
-image_path = os.path.join(os.getcwd(), image_name)
-
-if os.path.exists(image_path):
-    image_load = tf.keras.utils.load_img(image_path, target_size=(img_height, img_width))
-    img_arr = tf.keras.utils.img_to_array(image_load)  # Proper way to convert to array
-    img_bat = tf.expand_dims(img_arr, 0)  # Add batch dimension
-
-    prediction = model.predict(img_bat)
-    predicted_label = data_cat[np.argmax(prediction)]
-    confidence = np.max(prediction) * 100
-
-    st.image(image_path, width=200)
-    st.write(f" Prediction: **{predicted_label}**")
-    st.write(f" Confidence: **{confidence:.2f}%**")
-else:
-    st.error("❌ Image file not found. Make sure it's in the working directory.")
